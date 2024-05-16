c43358a871d80348d43393f926addd625a592616
Miriam Rayane Mendes da Silva <miriammendes@10-48-48-218.0200.ufpe.br.iosda.org>
2024-05-16 12:07:36 -0300

    add fonts and buttom

--- StyLeCode/StyLeCode/ContentView.swift
+++ StyLeCode/StyLeCode/ContentView.swift
@@ -25,7 +25,33 @@ struct ContentView: View {
                         .font(.custom("Merriweather-Black", size: 32))
                 }
                 Text("DESCUBRA SEU ESTILO DE APRENDIZADO")
+                    .foregroundColor(.secondarycolor)
+                    .font(.custom("Poppins-Bold", size: 16))
+                    .padding(.top, 48)
                 Text("Ao estudar, você costuma:")
+                    .foregroundColor(.secondarycolor)
+                    .font(.custom("Poppins-Regular", size: 16))
+                    .padding(.top, 32)
+                    .padding(.bottom, 20)
+                Rectangle()
+                    .frame(height: 444)
+                    .padding(.bottom, 28)
+                    .foregroundColor(.secondarycolor)
+                
+                Button(action: {print("Apertou o Botão")}, label: {
+                   
+                    ZStack {
+                        RoundedRectangle(cornerRadius: 24)
+                            .fill(.primarycolor)
+                                    .frame(width: 172, height: 46)
+                                    .background(
+                                        RoundedRectangle(cornerRadius: 25)
+                                            .stroke(Color.red, lineWidth: 6)
+                                    )
+                        Text("Calcular")
+                        
+                    }
+                })
             }
         }
         .ignoresSafeArea()
